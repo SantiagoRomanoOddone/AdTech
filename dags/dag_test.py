900f511e613ed75aed188a5c773c5ef541e8ad8a
SantiagoRomanoOddone <santiagoromano15@gmail.com>
2024-11-16 23:33:11 -0300

    adding dag_test

---
+++ dags/dag_test.py
@@ -0,0 +1,30 @@
+import datetime
+import numpy as np
+from airflow.models.dag import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from airflow.operators.empty import EmptyOperator
+from airflow.utils.task_group import TaskGroup
+
+
+
+
+with DAG(
+    dag_id='test',
+    schedule=None, # None means do not schedule
+    start_date=datetime.datetime(2024, 11, 5), # Start date of the DAG
+    catchup=False, # If start_date is in the past, it will run all the tasks between start_date and now. Take care of this parameter
+    ) as dag:
+        sleep = BashOperator( # BashOperator is a type of operator that executes a bash command
+            task_id='sleep', # Name of the task
+            bash_command='sleep 3', # Execute a bash command
+        )
+
+        def sample_normal(mean, std):
+            return np.random.normal(loc=mean, scale=std)
+        
+        random_height = PythonOperator( # PythonOperator is a type of operator that executes a Python function
+            task_id='height', # The name of the task.
+            python_callable=sample_normal, # The function to execute without the parameters
+            op_kwargs = {"mean" : 170, "std": 15}, # parameters to pass to the function
+        )
