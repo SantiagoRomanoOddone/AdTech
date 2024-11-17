4680ca4de1f3477aaca89ceb7408a4646acb41c8
SantiagoRomanoOddone <santiagoromano15@gmail.com>
2024-11-17 13:34:00 -0300

    adding model_pipeline working

--- dags/model_pipeline.py
+++ dags/model_pipeline.py
@@ -3,10 +3,12 @@ from airflow.models.dag import DAG
 from airflow.operators.python import PythonOperator
 import sys
 import os
+
 # Adding the parent directory of 'scr' to the Python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from scr.logic import (
+from scr.model import (
+    ensure_temp_folder_exists,
     filter_active_advertiser_views,
     filter_active_advertiser_products,
     compute_top_ctr,
@@ -14,9 +16,11 @@ from scr.logic import (
     write_to_db,
 )
 
+# Paths
 ads_views_path = 'data/ads_views.csv'
 advertiser_path = 'data/advertiser_ids.csv'
 product_views_path = 'data/product_views.csv'
+temp_folder = 'data/temp'
 
 # Define the DAG
 with DAG(
@@ -25,6 +29,15 @@ with DAG(
     start_date=datetime.datetime(2024, 11, 17),  # Start date of the DAG
     catchup=False,  # Do not backfill missing runs
 ) as dag:
+    # Task 0: Create temporary folder if it does not exist
+    temp_folder_task = PythonOperator(
+    task_id='create_temp_folder',
+    python_callable=ensure_temp_folder_exists,
+    op_kwargs={
+        'folder_path': temp_folder,
+    },
+    )
+
     # Task 1: Filter active advertiser views
     filter_views_task = PythonOperator(
         task_id='filter_active_advertiser_views',
@@ -32,6 +45,7 @@ with DAG(
         op_kwargs={
             'ads_views_path': ads_views_path,
             'advertiser_path': advertiser_path,
+            'output_folder': temp_folder,
         },
     )
 
@@ -42,6 +56,7 @@ with DAG(
         op_kwargs={
             'product_views_path': product_views_path,
             'advertiser_path': advertiser_path,
+            'output_folder': temp_folder,
         },
     )
 
@@ -50,7 +65,8 @@ with DAG(
         task_id='compute_top_ctr',
         python_callable=compute_top_ctr,
         op_kwargs={
-            'active_ads_views': "{{ ti.xcom_pull(task_ids='filter_active_advertiser_views') }}",
+            'active_ads_views_path': "{{ ti.xcom_pull(task_ids='filter_active_advertiser_views') }}",
+            'output_folder': temp_folder,
         },
     )
 
@@ -59,24 +75,23 @@ with DAG(
         task_id='compute_top_products',
         python_callable=compute_top_product,
         op_kwargs={
-            'active_product_views': "{{ ti.xcom_pull(task_ids='filter_active_advertiser_products') }}",
+            'active_product_views_path': "{{ ti.xcom_pull(task_ids='filter_active_advertiser_products') }}",
+            'output_folder': temp_folder,
         },
     )
 
-    # Task 5: Write results to the database
-    write_to_db_task = PythonOperator(
-        task_id='write_to_db',
-        python_callable=write_to_db,
-        op_kwargs={
-            'top_ctr_path': "{{ ti.xcom_pull(task_ids='compute_top_ctr') }}",
-            'top_product_path': "{{ ti.xcom_pull(task_ids='compute_top_products') }}",
-        },
-    )
+    # # Task 5: Write results to the database
+    # write_to_db_task = PythonOperator(
+    #     task_id='write_to_db',
+    #     python_callable=write_to_db,
+    #     op_kwargs={
+    #         'top_ctr_path': "{{ ti.xcom_pull(task_ids='compute_top_ctr') }}",
+    #         'top_product_path': "{{ ti.xcom_pull(task_ids='compute_top_products') }}",
+    #     },
+    # )
 
     # Define dependencies
+    temp_folder_task >> [filter_views_task, filter_products_task]
     filter_views_task >> compute_top_ctr_task
     filter_products_task >> compute_top_product_task
-    [compute_top_ctr_task, compute_top_product_task] >> write_to_db_task
-
-
-
+    #[compute_top_ctr_task, compute_top_product_task] >> write_to_db_task
